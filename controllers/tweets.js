508539df42315b4173a954f2e5d0c5cdf9b4d199
hdmcgehee <hdmcgehee@gmail.com>
2020-09-25 17:26:23 -0400

    api call method

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,10 +1,22 @@
 const db = require('../models/index')
+const grabTweets = require('../getTweetsById')
+let visitNum = 0;
 
 const index = (req, res) => {
+
+  // console.log(grabTweets.getTweets('25073877'));
+
+  if(visitNum % 5 === 0){
+    grabTweets.getTweets('25073877');
+  }
+
   db.Tweet.find({}, (err, foundTweets) => {
     if (err) console.log('Error at Tweet index', err);
     res.status(200).json(foundTweets)
   })
+  console.log(visitNum);
+  visitNum++;
+
 }
 
 // const create = (req, res) => {
---
+++ getTweetsById.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+require('dotenv').config();
+const db = require('./models/index');
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.twitterAuthToken}`
+  }
+}
+
+module.exports = {
+    getTweets: function (userId){
+
+        axios.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=${userId}&include_rts=false&count=20`, config)
+          .then((res) => {
+        
+            const tweetData = {};
+        
+            for(let i = 0; i < res.data.length; i++){
+                tweetData.id = res.data[i].id;
+                tweetData.text = res.data[i].text;
+                tweetData.created_at = res.data[i].created_at;
+                tweetData.username = res.data[i].user.screen_name;
+                tweetData.full_name = res.data[i].user.name;
+                tweetData.user_img = res.data[i].user.profile_image_url;
+                
+        
+                db.Tweet.create(tweetData, (err, newTweet) => {
+                    if (err) console.log('Error at getTweets create', err);
+                    // console.log(newTweet)
+                  })
+            }
+          })
+          .catch((err) => console.log(err))
+    }
+};
